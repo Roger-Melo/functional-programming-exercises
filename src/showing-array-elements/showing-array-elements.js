ee89a3a9a6bb5d8927904cfb73aed822fb8d5b8a
Roger Melo <rogerw592@gmail.com>
2018-12-14 15:24:37 -0200

    add functional solution

--- src/showing-array-elements/showing-array-elements.js
+++ src/showing-array-elements/showing-array-elements.js
@@ -3,11 +3,19 @@
 const showElements = (arr) => {
   if (!Array.isArray(arr))
     throw new TypeError('The parameter must be an array')
-  
+
   if (arr.length === 0)
     throw new SyntaxError('The array must not be empty')
+  
+  const str = 'The elements in array are '
 
-  return `The elements in array are ${arr.join(', ')}`
+  return (function showElementsInternal (arrInternal, strInternal) {
+    const [head, ...tail] = arrInternal
+
+    return arrInternal.length === 0
+      ? strInternal.slice(0, -2)
+      : showElementsInternal(tail, strInternal.concat(`${head}, `))
+  })(arr, str)
 }
 
 export default showElements
